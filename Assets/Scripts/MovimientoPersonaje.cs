10eb0145c12f4a037a262610da16682de8f38d63
Mario Cordova <mcordovacsx@gmail.com>
2025-10-12 16:56:50 -0600

    Se agrega el salto al personaje

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -8,9 +8,10 @@ public class MovimientoPersonaje: MonoBehaviour
     public float Velocidad = 15f;
     public float Gravedad = -9.81f;
     public Transform Piso;
-    public float DistanciaPiso;
+    public float DistanciaPiso = 0.5f;
     public LayerMask CapaPiso;
-    
+    public float AlturaSalto = 2f;
+
 
     Vector3 VelocidadVertical;
     bool EstaEnPiso;
@@ -33,6 +34,12 @@ public class MovimientoPersonaje: MonoBehaviour
 
         Vector3 mover = transform.right * x + transform.forward * z;
         Controlador.Move(mover * Velocidad * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && EstaEnPiso)
+        {
+            VelocidadVertical.y = Mathf.Sqrt(AlturaSalto * -2f * Gravedad);
+            Debug.Log("Saltando! Velocidad Y: " + VelocidadVertical.y);
+        }
 
         VelocidadVertical.y += Gravedad * Time.deltaTime;
         Controlador.Move(VelocidadVertical * Time.deltaTime);
